162c289ef995e935b14854fac6e40eb36628a762
Jtobyy <jtobi8161@gmail.com>
2021-11-20 16:13:22 +0100

    First Commit and Website Authentication

--- pictures_website/dgallery/urls.py
+++ pictures_website/dgallery/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = "dgallery"
+
 urlpatterns = [
-    path('login', views.login, name = 'login'),
+    path('login', views.login_view, name = 'login'),
     path('register', views.register, name = 'register'),
 ]
--- pictures_website/dgallery/views.py
+++ pictures_website/dgallery/views.py
@@ -1,7 +1,32 @@
-from django.shortcuts import render
+from django.conf.urls import url
+from django.contrib import messages
+from django.contrib.messages.constants import ERROR, SUCCESS
+from django.http.response import HttpResponse
+from django.shortcuts import render, redirect
+from .forms import RegForm
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+from django.http import HttpResponse
 
-def login(request):
-    return render(request, 'dgallery/index.html', None)
 
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.add_message(request, SUCCESS, "Login successful.")
+            return HttpResponse("Thanks for testing")
+    return render(request, 'dgallery/login.html', None)
+        
 def register(request):
-    pass
+    if request.method == "POST":
+        form = RegForm(request.POST)    
+        if form.is_valid():
+            form.save()
+            messages.add_message(request, SUCCESS, "Registration successful.")
+            return redirect("/dgallery/login", permanent=True)
+        messages.add_message(request, ERROR, "Unsuccessful registration, Invalid information.")
+    form = RegForm()
+    return render (request, "dgallery/register.html", {'form': form})
